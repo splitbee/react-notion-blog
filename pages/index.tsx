2f48953889ffa216e60f8e305028345bac4eecf3
Buğra Canata <bcanata@hotmail.com>
2020-06-21 19:04:59 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-const NOTION_BLOG_ID = "1099525da7e5405c961706de56622ccd";
+const NOTION_BLOG_ID = "aabce17bf80a4735a699d60fce2c801b";
 
 export type Post = { id: string; slug: string; title: string; date: string };
 
 export const getAllPosts = async (): Promise<Post[]> => {
   return await fetch(
-    `https://notion-api.splitbee.io/v1/table/${NOTION_BLOG_ID}`
+    `https://api.notlar.im/v1/table/${NOTION_BLOG_ID}`
   ).then((res) => res.json());
 };
 
