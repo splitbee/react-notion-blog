76a494d479734cc6ee101bac44fc41478b56387d
Tobias Lins <me@tobi.sh>
2020-07-23 22:45:17 +0200

    allow environment variable for blog

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
-const NOTION_BLOG_ID = "1099525da7e5405c961706de56622ccd";
+const NOTION_BLOG_ID =
+  process.env.NOTION_BLOG_ID || "1099525da7e5405c961706de56622ccd";
 
 export type Post = { id: string; slug: string; title: string; date: string };
 
